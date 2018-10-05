8918ed836ef63acd5943e52c0abe0cbca1fc3516
slalwani7 <38328014+slalwani7@users.noreply.github.com>
2018-10-05 10:37:20 -0700

    Multiply File

---
+++ src/multiply/java/multiply.java
@@ -0,0 +1,12 @@
+package multiply.java;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 7;
+		int z = x * y;
+		System.out.println("multiplication: " + z);
+	}
+	
+}
